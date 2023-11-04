1a84b7e98a95524707ff674859750171fd19034f
unknown <oppongcoffie27@gmail.com>
2023-11-04 18:37:55 +0000

    multiple players

--- index.js
+++ index.js
@@ -423,7 +423,7 @@ form.on('submit', function (event) {
     memoryDiv.append(logo);
 
     function handledivs() {
-      const numbers = ["anchor", "flask", "hand-spock", "certificate", "snowflake", "sun", "moon", "hourglass-half", "expand-arrows-alt", "retweet", "sync", "compress-arrows-alt", "random", "balance-scale", "apple-alt", "graduation-cap", "wifi", "euro-sign", "anchor", "flask", "hand-spock", "futbal", "snowflake", "sun", "moon", "hourglass-half", "expand-arrows-alt", "retweet", "sync", "compress-arrows-alt", "random", "balance-scale", "apple-alt", "graduation-cap", "wifi", "euro-sign"];
+      const numbers = ["anchor", "flask", "hand-spock", "trophy", "snowflake", "sun", "moon", "hourglass-half", "expand-arrows-alt", "retweet", "sync", "compress-arrows-alt", "random", "balance-scale", "apple-alt", "graduation-cap", "wifi", "euro-sign", "anchor", "flask", "hand-spock", "trophy", "snowflake", "sun", "moon", "hourglass-half", "expand-arrows-alt", "retweet", "sync", "compress-arrows-alt", "random", "balance-scale", "apple-alt", "graduation-cap", "wifi", "euro-sign"];
       var shuf_numbers = numbers.sort(() => (Math.random() > .5) ? 2 : -1);
       for (var i = 0; i < numbers.length; i++) {
         let box = document.createElement('div');
@@ -443,8 +443,15 @@ form.on('submit', function (event) {
                 document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
 
                 if (document.querySelectorAll('.boxMatch').length == numbers.length) {
-                  alert('WIN');
-                }
+// WHEN ALL BOXES ARE OPENED;
+main1.style.backgroundColor='rgba(0, 0, 0, 0.5)';
+main1.style.backdropFilter = 'blur(5px)';
+winContainer.style.marginTop = '-37rem';
+main1.style.backgroundColor='rgba(0, 0, 0, 0.5)';
+main1.style.backdropFilter = 'blur(5px)';
+winContainer.style.marginTop = '-37rem';
+clearInterval(intervalId);
+isrunning = false;                }
               } else {
                 document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
                 document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
@@ -498,6 +505,51 @@ form.on('submit', function (event) {
     currentDiv.classList.add('current');
     currentDiv.innerText = 'Current Turn';
 
+      // START:: Result
+      let winContainer = document.createElement('div');
+      winContainer.classList.add('wincontainer');
+      let didit = document.createElement('div');
+      let didtop = document.createElement('div');
+      didtop.classList.add('didtop');
+      didtop.innerText = 'YOU DID IT'
+      let diddown = document.createElement('div');
+      diddown.classList.add('diddown');
+      diddown.innerText = 'Game over! Here"s how you got on...';
+      didit.append(didtop);
+      didit.append(diddown);
+      winContainer.append(didit);
+  
+      // loop for players and their scores
+      for(let i = 0; i<4; i++){
+      let elapse = document.createElement('div');
+      elapse.classList.add('elapse');
+      let elapsetitle = document.createElement('div');
+      elapsetitle.classList.add('elapsetitle');
+      elapsetitle.innerText = 'Time Elapsed';
+      elapse.append(elapsetitle);
+      let elapsetime = document.createElement('div');
+      elapsetime.classList.add('elapsetitletime');
+      elapsetime.innerText = '1:52';
+      elapse.append(elapsetime);
+      winContainer.append(elapse);
+      }
+  
+  
+      let winstarts = document.createElement('div');
+      winstarts.classList.add('winstarts');
+      let winrestart = document.createElement('div');
+      winrestart.classList.add('winrestart');
+      winrestart.innerText = 'Restart';
+      winstarts.append(winrestart);
+      let winsetup = document.createElement('div');
+      winsetup.classList.add('winsetup');
+      winsetup.innerText = 'Setup New Game';
+      winstarts.append(winsetup);
+      
+      winContainer.append(winstarts);
+          // END:: Result
+
+
     // Remove existing content from the "main" element
     overall.empty();
 
@@ -505,6 +557,7 @@ form.on('submit', function (event) {
     // Append the new div elements to the "main" element
     overall.append(main1);
     main1.append(top, middle, down, currentDiv);
+    overall.append(winContainer);
   }
   // END:: Icons, multiple users and 6x6 grid checked
 
@@ -690,7 +743,15 @@ form.on('submit', function (event) {
                 document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
 
                 if (document.querySelectorAll('.boxMatch').length == numbers.length) {
-                  alert('WIN');
+// WHEN ALL BOXES ARE OPENED;
+                   main1.style.backgroundColor='rgba(0, 0, 0, 0.5)';
+                  main1.style.backdropFilter = 'blur(5px)';
+                  winContainer.style.marginTop = '-37rem';
+                  main1.style.backgroundColor='rgba(0, 0, 0, 0.5)';
+                  main1.style.backdropFilter = 'blur(5px)';
+                  winContainer.style.marginTop = '-37rem';
+                  clearInterval(intervalId);
+                  isrunning = false;
                 }
               } else {
                 document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
@@ -745,6 +806,50 @@ form.on('submit', function (event) {
     currentDiv.classList.add('current');
     currentDiv.innerText = 'Current Turn';
 
+        // START:: Result
+        let winContainer = document.createElement('div');
+        winContainer.classList.add('wincontainer');
+        let didit = document.createElement('div');
+        let didtop = document.createElement('div');
+        didtop.classList.add('didtop');
+        didtop.innerText = 'YOU DID IT'
+        let diddown = document.createElement('div');
+        diddown.classList.add('diddown');
+        diddown.innerText = 'Game over! Here"s how you got on...';
+        didit.append(didtop);
+        didit.append(diddown);
+        winContainer.append(didit);
+    
+        // loop for players and their scores
+        for(let i = 0; i<4; i++){
+        let elapse = document.createElement('div');
+        elapse.classList.add('elapse');
+        let elapsetitle = document.createElement('div');
+        elapsetitle.classList.add('elapsetitle');
+        elapsetitle.innerText = 'Time Elapsed';
+        elapse.append(elapsetitle);
+        let elapsetime = document.createElement('div');
+        elapsetime.classList.add('elapsetitletime');
+        elapsetime.innerText = '1:52';
+        elapse.append(elapsetime);
+        winContainer.append(elapse);
+        }
+    
+    
+        let winstarts = document.createElement('div');
+        winstarts.classList.add('winstarts');
+        let winrestart = document.createElement('div');
+        winrestart.classList.add('winrestart');
+        winrestart.innerText = 'Restart';
+        winstarts.append(winrestart);
+        let winsetup = document.createElement('div');
+        winsetup.classList.add('winsetup');
+        winsetup.innerText = 'Setup New Game';
+        winstarts.append(winsetup);
+        
+        winContainer.append(winstarts);
+            // END:: Result
+
     // Remove existing content from the "main" element
     overall.empty();
 
@@ -752,13 +857,16 @@ form.on('submit', function (event) {
     // Append the new div elements to the "main" element
     overall.append(main1);
     main1.append(top, middle, down, currentDiv);
+        overall.append(winContainer);
+
   }
   // END:: Icons, multiple users and 4x4 grid checked
 
 
   // START:: Numbers, 1 users and 4x4 grid checked
   else if (numbers.is(':checked') && fxf.is(':checked') && player1.is(':checked')) {
-    let numberOfPlayers;
+    var numberOfMoves = 0;
+    let intervalId;
     if (player4.is(':checked')) {
       numberOfPlayers = 4;
     } else if (player3.is(':checked')) {
@@ -802,7 +910,21 @@ form.on('submit', function (event) {
         let box = document.createElement('div');
         box.className = 'item';
         box.innerHTML = shuf_numbers[i];
+        var sec = 0;
+        var isrunning = false;
         box.onclick = function () {
+          if (!isrunning) {
+            isrunning = true;
+            intervalId = setInterval(() => {
+              time.innerText = '00:' + sec.toString().padStart(2, '0');
+              elapsetime.innerText = '00:' + sec.toString().padStart(2, '0');
+              sec++;
+            }, 1000);
+          }
+          numberOfMoves += 0.5;
+          moves.innerText = Math.floor(numberOfMoves);
+          movestaken_time.innerText = numberOfMoves + 'Moves';
+
           this.classList.add('boxOpen');
           setTimeout(function () {
             if (document.querySelectorAll('.boxOpen').length > 1) {
@@ -812,10 +934,13 @@ form.on('submit', function (event) {
 
                 document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
                 document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
-
+// WHEN ALL BOXES ARE OPENED
                 if (document.querySelectorAll('.boxMatch').length == numbers.length) {
-                  alert('WIN');
-                }
+                  main1.style.backgroundColor='rgba(0, 0, 0, 0.5)';
+                  main1.style.backdropFilter = 'blur(5px)';
+                  winContainer.style.marginTop = '-37rem';  
+                  clearInterval(intervalId);
+                  isrunning = false;              }
               } else {
                 document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
                 document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
@@ -840,7 +965,7 @@ form.on('submit', function (event) {
 
     let time = document.createElement('div');
     time.classList.add('time');
-    time.innerText = '02';
+    time.innerText = '0:00';
 
     timeDiv.append(timetitle);
     timeDiv.append(time);
@@ -855,7 +980,7 @@ form.on('submit', function (event) {
 
     let moves = document.createElement('div');
     moves.classList.add('moves');
-    moves.innerText = '7';
+    moves.innerText = '0';
 
     movesDiv.append(movestitle);
     movesDiv.append(moves);
@@ -865,6 +990,53 @@ form.on('submit', function (event) {
     down.appendChild(movesDiv); // Add movesDiv to "down" element
 
 
+    // START:: Result
+    let winContainer = document.createElement('div');
+    winContainer.classList.add('wincontainer');
+    let didit = document.createElement('div');
+    let didtop = document.createElement('div');
+    didtop.classList.add('didtop');
+    didtop.innerText = 'YOU DID IT'
+    let diddown = document.createElement('div');
+    diddown.classList.add('diddown');
+    diddown.innerText = 'Game over! Here"s how you got on...';
+    didit.append(didtop);
+    didit.append(diddown);
+
+    let elapse = document.createElement('div');
+    elapse.classList.add('elapse');
+    let elapsetitle = document.createElement('div');
+    elapsetitle.classList.add('elapsetitle');
+    elapsetitle.innerText = 'Time Elapsed';
+    elapse.append(elapsetitle);
+    let elapsetime = document.createElement('div');
+    elapsetime.classList.add('elapsetitletime');
+    elapse.append(elapsetime);
+
+    let movestaken = document.createElement('div');
+    movestaken.classList.add('movestaken');
+    let movestaken_title = document.createElement('div');
+    movestaken_title.classList.add('movestaken_time');
+    movestaken.innerText = 'Moves Taken';
+    movestaken.append(movestaken_title);
+    let movestaken_time = document.createElement('div');
+    movestaken_time.classList.add('movestaken_time');
+    movestaken.append(movestaken_time);
+
+    let winstarts = document.createElement('div');
+    winstarts.classList.add('winstarts');
+    let winrestart = document.createElement('div');
+    winrestart.classList.add('winrestart');
+    winrestart.innerText = 'Restart';
+    winstarts.append(winrestart);
+    let winsetup = document.createElement('div');
+    winsetup.classList.add('winsetup');
+    winsetup.innerText = 'Setup New Game';
+    winstarts.append(winsetup);
+    
+
+    winContainer.append(didit, elapse, movestaken, winstarts)
+
     // Remove existing content from the "main" element
     overall.empty();
 
@@ -872,6 +1044,7 @@ form.on('submit', function (event) {
     // Append the new div elements to the "main" element
     overall.append(main1);
     main1.append(top, middle, down);
+    overall.append(winContainer);
   }
   // END:: Numbers, 1 users and 4x4 grid checked
 
@@ -915,7 +1088,7 @@ form.on('submit', function (event) {
 
     memoryDiv.append(logo);
 
-    // START:: The middle div
+    //The middle div
     function handledivs() {
       const numbers = ["anchor", "flask", "hand-spock", "sun", "hourglass-half", "moon", "random", "apple-alt", "anchor", "flask", "hand-spock", "sun", "hourglass-half", "moon", "random", "apple-alt"];
       var shuf_numbers = numbers.sort(() => (Math.random() > .5) ? 2 : -1);
@@ -931,7 +1104,7 @@ form.on('submit', function (event) {
         box.onclick = function () {
           if (!isrunning) {
             isrunning = true;
-            setInterval(() => {
+            intervalId = setInterval(() => {
               time.innerText = '00:' + sec.toString().padStart(2, '0');
               sec++;
             }, 1000);
@@ -952,9 +1125,13 @@ form.on('submit', function (event) {
 
                 document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
                 document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
-
+// WHEN ALL BOXES ARE OPENED
                 if (document.querySelectorAll('.boxMatch').length == numbers.length) {
-                  // main1.style.opacity = 0.5;
+                  main1.style.backgroundColor='rgba(0, 0, 0, 0.5)';
+                  main1.style.backdropFilter = 'blur(5px)';
+                  winContainer.style.marginTop = '-37rem';
+                  clearInterval(intervalId);
+                  isrunning = false;
                 }
               } else {
                 document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
@@ -1022,22 +1199,23 @@ form.on('submit', function (event) {
     elapsetitle.innerText = 'Time Elapsed';
     elapse.append(elapsetitle);
     let elapsetime = document.createElement('div');
-    elapsetime.classList.add('elapsetitle');
-    elapsetime.innerText = '0052';
+    elapsetime.classList.add('elapsetitletime');
+    elapsetime.innerText = '1:52';
     elapse.append(elapsetime);
 
     let movestaken = document.createElement('div');
     movestaken.classList.add('movestaken');
     let movestaken_title = document.createElement('div');
-    movestaken_title.classList.add('movestaken_title');
+    movestaken_title.classList.add('movestaken_time');
     movestaken.innerText = 'Moves Taken';
     movestaken.append(movestaken_title);
     let movestaken_time = document.createElement('div');
     movestaken_time.classList.add('movestaken_time');
     movestaken_time.innerText = '72 Moves';
-    elapse.append(movestaken_time);
+    movestaken.append(movestaken_time);
 
     let winstarts = document.createElement('div');
+    winstarts.classList.add('winstarts');
     let winrestart = document.createElement('div');
     winrestart.classList.add('winrestart');
     winrestart.innerText = 'Restart';
@@ -1045,10 +1223,11 @@ form.on('submit', function (event) {
     let winsetup = document.createElement('div');
     winsetup.classList.add('winsetup');
     winsetup.innerText = 'Setup New Game';
-    winsetup.append(winrestart);
+    winstarts.append(winsetup);
     
+    winContainer.append(didit, elapse, movestaken, winstarts)
+        // END:: Result
 
-    winContainer.append(didit, elapse, movestaken)
 
     // Remove existing content from the "main" element
     overall.empty();
@@ -1056,11 +1235,10 @@ form.on('submit', function (event) {
 
     // Append the new div elements to the "main" element
     overall.append(main1);
-    main1.append(top, middle, down, winContainer);
+    main1.append(top, middle, down);
+    overall.append(winContainer);
   }
   // END:: Icons, 1 users and 4x4 grid checked
-
-
 
 });
 
